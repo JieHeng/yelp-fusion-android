42d4a0ed22f906033d1e3e32c95b8ff6a49cd542
Jie Heng <jiehenglim@gmail.com>
2019-06-25 14:47:35 +0800

    update default transaction type

--- yelpfusion/src/main/java/com/jieheng/yelpfusion/request/TransactionSearchRequest.kt
+++ yelpfusion/src/main/java/com/jieheng/yelpfusion/request/TransactionSearchRequest.kt
@@ -1,7 +1,7 @@
 package com.jieheng.yelpfusion.request
 
 class TransactionSearchRequest(
-    val transactionType: String = "food delivery",
+    val transactionType: String = "delivery",
     val location: String? = null,
     val latitude: Double? = null,
     val longitude: Double? = null
